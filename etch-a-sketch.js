83576e64971d9ecddf68aa516bb1da0a2d9289cd
Jasmine <jasmineallen1107@gmail.com>
2021-07-21 10:16:26 -0500

    Interative grid/input range created

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,28 +1,62 @@
-//Make multiple divs.
-//^Repeat a createElement and append
-//loop until it's the set number.
-//Default is 256 individual divs.
-//Must be made into a square. (16x16)
-//^^ Achieve this by having a set
-//div for them to fill,
-//using flex.
 
-//Need number input. Tells loop how many times to loop.
+let slider = document.getElementById('myRange');
+let sliderNum =  4;
+let dataList = document.getElementById('dataList');
 
-let sliderNum = 256;
+function getClosest(arr, valueOfSlider) {
+	return arr.reduce(function (prev, curr) { //Snaps to the closest (least difference) square in array.
+    return (Math.abs(curr - valueOfSlider) < Math.abs(prev - valueOfSlider) ? curr : prev);
+  });
+}
 
-let rowAndColumnNum = Math.sqrt(sliderNum);
+slider.addEventListener('change', function () {
+    sliderNum = document.getElementById('myRange').value;
+    let optionArr = []; //Array that will allow snapping to values.
+    for (i = 4; i < 1000; i ++) {
+        if (Math.sqrt(i) % 1 == 0) {
+            optionArr.push(i);
+        }
+    }
+    sliderNumSqrt = getClosest(optionArr, sliderNum);
+    console.log(optionArr);
+    if (Math.sqrt(sliderNumSqrt) % 1 == 0) {
+        let rowAndColumnNum = Math.sqrt(sliderNumSqrt);
+        canvas.style.gridTemplateRows = `repeat(${rowAndColumnNum}, 1fr)`;
+        canvas.style.gridTemplateColumns = `repeat(${rowAndColumnNum}, 1fr)`;
+        canvas.innerHTML = '';
+        for (i = 0; i < sliderNumSqrt; i++) {
+            let lilDiv = document.createElement('div');
+            lilDiv.style.cssText = `
+                background-color: white;
+                border: .1px solid gray;
+            `;
+        canvas.appendChild(lilDiv);
+        }
+    }
+})
+
+
+let stepList = () => {
+    for (j = 4; j < 1000; j++) {
+        if (Math.sqrt(j) % 1 == 0) {
+            let newOption = document.createElement('option');
+            newOption.innerHTML = j;
+            dataList.appendChild(newOption);
+        }
+    }
+}
+
+stepList();
+
+console.log(dataList);
+
+slider.list = dataList;
+
+
 
 let canvas = document.getElementById('canvas');
 
-for (i = 0; i < sliderNum; i++) {
-    let lilDiv = document.createElement('div');
-    lilDiv.style.cssText = `
-        background-color: white;
-        border: .1px solid gray;
-    `;
-    canvas.appendChild(lilDiv);
-}
 
-canvas.style.gridTemplateRows = `repeat(${rowAndColumnNum}, 1fr)`;
-canvas.style.gridTemplateColumns = `repeat(${rowAndColumnNum}, 1fr)`;
+
+
+
