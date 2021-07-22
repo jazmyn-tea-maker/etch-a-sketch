92533bb90168054937ffa0709b7ec155ca60ac12
Jasmine <jasmineallen1107@gmail.com>
2021-07-22 07:59:28 -0500

    Text-input and default canvas onload created

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,24 +1,69 @@
 
 let slider = document.getElementById('myRange');
-let sliderNum =  4;
+let sliderNum =  4; // Default size.
 let dataList = document.getElementById('dataList');
+let canvas = document.getElementById('canvas');
 
+// Setting up default size:
+let rowAndColumnNum = Math.sqrt(sliderNum);
+canvas.style.gridTemplateRows = `repeat(${rowAndColumnNum}, 1fr)`;
+canvas.style.gridTemplateColumns = `repeat(${rowAndColumnNum}, 1fr)`;
+for (k = 0; k < sliderNum; k++) {
+    let lilDiv = document.createElement('div');
+    lilDiv.style.cssText = `
+        background-color: white;
+        border: .1px solid gray;
+    `;
+    canvas.appendChild(lilDiv);
+}
+
+// Snaps to the closest (least difference) square in array with
+// all squares under 1000:
 function getClosest(arr, valueOfSlider) {
-	return arr.reduce(function (prev, curr) { //Snaps to the closest (least difference) square in array.
+	return arr.reduce(function (prev, curr) { 
     return (Math.abs(curr - valueOfSlider) < Math.abs(prev - valueOfSlider) ? curr : prev);
   });
 }
 
-slider.addEventListener('change', function () {
+//If the user inputs text, use that.
+let userInput = document.getElementById('size-input');
+
+userInput.addEventListener('change', userCustomSize = () => {
+    let userInputSize = parseInt(document.getElementById('size-input').value) // Turns the input into a number.
+    let optionArr = []; // Array that holds values available (to snap to).
+    for (i = 4; i < 1000; i ++) {
+        if (Math.sqrt(i) % 1 == 0) {
+            optionArr.push(i);
+        }
+    }
+    userInputSqrt = getClosest(optionArr, userInputSize);
+    if (Math.sqrt(userInputSqrt) % 1 == 0) {
+        let rowAndColumnNum = Math.sqrt(userInputSqrt);
+        canvas.style.gridTemplateRows = `repeat(${rowAndColumnNum}, 1fr)`;
+        canvas.style.gridTemplateColumns = `repeat(${rowAndColumnNum}, 1fr)`;
+        canvas.innerHTML = '';
+        for (i = 0; i < userInputSqrt; i++) {
+            let lilDiv = document.createElement('div');
+            lilDiv.style.cssText = `
+                background-color: white;
+                border: .1px solid gray;
+            `;
+        canvas.appendChild(lilDiv);
+        }
+    }
+    console.log(userInputSize);
+})
+
+// If the user slides the bar, use the value from that.
+slider.addEventListener('change', changeCanvasSize = () => {
     sliderNum = document.getElementById('myRange').value;
-    let optionArr = []; //Array that will allow snapping to values.
+    let optionArr = []; // Array that holds values available (to snap to).
     for (i = 4; i < 1000; i ++) {
         if (Math.sqrt(i) % 1 == 0) {
             optionArr.push(i);
         }
     }
     sliderNumSqrt = getClosest(optionArr, sliderNum);
-    console.log(optionArr);
     if (Math.sqrt(sliderNumSqrt) % 1 == 0) {
         let rowAndColumnNum = Math.sqrt(sliderNumSqrt);
         canvas.style.gridTemplateRows = `repeat(${rowAndColumnNum}, 1fr)`;
@@ -50,12 +95,3 @@ stepList();
 console.log(dataList);
 
 slider.list = dataList;
-
-
-
-let canvas = document.getElementById('canvas');
-
-
-
-
-
