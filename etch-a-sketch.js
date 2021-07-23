51e55a0b749a3f9086b054b9a8a6434212fd0258
Jasmine <jasmineallen1107@gmail.com>
2021-07-23 08:53:03 -0500

    Event Listeners to allow drawing added. Note: still buggy. Lags bad.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -7,31 +7,28 @@ let htmlStyle = document.getElementsByTagName('style');
 let divColor = document.getElementById('color-selector');
 
 // Setting up default size:
-let rowAndColumnNum = Math.sqrt(sliderNum);
+let rowAndColumnNum = sliderNum;
 canvas.style.gridTemplateRows = `repeat(${rowAndColumnNum}, 1fr)`;
 canvas.style.gridTemplateColumns = `repeat(${rowAndColumnNum}, 1fr)`;
-for (k = 0; k < sliderNum; k++) {
+for (k = 0; k < sliderNum ** 2; k++) {
     let lilDiv = document.createElement('div');
     lilDiv.id = `div${k}`;
     lilDiv.style.cssText = `
         background-color: white;
         border: .1px solid gray;
     `;
-    lilDiv.addEventListener('mouseenter', function(e) {
+    function hoverFunc(e) {
         e.target.style.backgroundColor = divColor.value;
+    }
+    canvas.addEventListener('mousedown', function clickHoldFunc(e) {
+        lilDiv.addEventListener('mouseenter', hoverFunc);
+        canvas.addEventListener('mouseup', function(e) {
+            lilDiv.removeEventListener('mouseenter', hoverFunc);
+            e.target.removeEventListener('mousedown', clickHoldFunc);
+        })
     });
-    canvas.appendChild(lilDiv);
+    canvas.appendChild(lilDiv); 
 }
-
-// Grid needs squares to fit into square div.
-// Snaps to the closest (least difference) square in array with
-// all squares under 1000:
-function getClosest(arr, valueOfSlider) {
-	return arr.reduce(function (prev, curr) { 
-    return (Math.abs(curr - valueOfSlider) < Math.abs(prev - valueOfSlider) ? curr : prev);
-  });
-}
-
 
 let userInput = document.getElementById('size-input'); 
 
@@ -40,28 +37,29 @@ userInput.addEventListener('keydown', userCustomSize = (e) => {
 
     if (e.key == 'Enter' || e.eventCode == 13) {
         let userInputSize = parseInt(userInput.value);// Turns the input into a number.
-        let optionArr = []; // Array that holds values available (to snap to).
-        for (i = 4; i < 1000; i ++) {
-            if (Math.sqrt(i) % 1 == 0) { // If the square root of a num isn't divisible by 1 with a 
-                optionArr.push(i); // remainder of 0, it is not a square.
-            }
+        if (userInputSize >= 91) {
+            userInputSize = 90;
         }
-        userInputSqrt = getClosest(optionArr, userInputSize);
-        if (Math.sqrt(userInputSqrt) % 1 == 0) {
-            let rowAndColumnNum = Math.sqrt(userInputSqrt);
+        if (userInputSize < 91) {
+            let rowAndColumnNum = userInputSize;
             canvas.style.gridTemplateRows = `repeat(${rowAndColumnNum}, 1fr)`;
             canvas.style.gridTemplateColumns = `repeat(${rowAndColumnNum}, 1fr)`;
             canvas.innerHTML = '';
-            for (i = 0; i < userInputSqrt; i++) {
+            for (i = 0; i < userInputSize ** 2; i++) {
                 let lilDiv = document.createElement('div');
                 lilDiv.style.cssText = `
                     background-color: white;
                     border: .1px solid gray;
                 `;
-                lilDiv.addEventListener('mouseenter', function(e) {
+                function hoverFunc(e) {
                     e.target.style.backgroundColor = divColor.value;
-                    // When the cursor 'hovers'/'enters' a div, it will
-                    // change color.
+                }
+                canvas.addEventListener('mousedown', function clickHoldFunc(e) {
+                    lilDiv.addEventListener('mouseenter', hoverFunc);
+                    canvas.addEventListener('mouseup', function(e) {
+                        lilDiv.removeEventListener('mouseenter', hoverFunc);
+                        e.target.removeEventListener('mousedown', clickHoldFunc);
+                    })
                 });
                 canvas.appendChild(lilDiv);
             }
@@ -75,45 +73,27 @@ userInput.addEventListener('keydown', userCustomSize = (e) => {
 slider.addEventListener('input', changeCanvasSize = () => {
 
     sliderNum = document.getElementById('myRange').value;
-    let optionArr = []; // Array that holds values available (to snap to).
-    for (i = 4; i < 1000; i ++) {
-        if (Math.sqrt(i) % 1 == 0) {
-            optionArr.push(i);
+    let rowAndColumnNum = sliderNum;
+    canvas.style.gridTemplateRows = `repeat(${rowAndColumnNum}, 1fr)`;
+    canvas.style.gridTemplateColumns = `repeat(${rowAndColumnNum}, 1fr)`;
+    canvas.innerHTML = '';
+    for (i = 0; i < sliderNum ** 2; i++) {
+        let lilDiv = document.createElement('div');
+        lilDiv.style.cssText = `
+            background-color: white;
+            border: .1px solid gray;
+        `;
+        function hoverFunc(e) {
+            e.target.style.backgroundColor = divColor.value;
         }
-    }
-    sliderNumSqrt = getClosest(optionArr, sliderNum);
-    if (Math.sqrt(sliderNumSqrt) % 1 == 0) {
-        let rowAndColumnNum = Math.sqrt(sliderNumSqrt);
-        canvas.style.gridTemplateRows = `repeat(${rowAndColumnNum}, 1fr)`;
-        canvas.style.gridTemplateColumns = `repeat(${rowAndColumnNum}, 1fr)`;
-        canvas.innerHTML = '';
-        for (i = 0; i < sliderNumSqrt; i++) {
-            let lilDiv = document.createElement('div');
-            lilDiv.style.cssText = `
-                background-color: white;
-                border: .1px solid gray;
-            `;
-            lilDiv.addEventListener('mouseenter', function(e) {
-                e.target.style.backgroundColor = divColor.value;
-            });
+        canvas.addEventListener('mousedown', function clickHoldFunc(e) {
+            lilDiv.addEventListener('mouseenter', hoverFunc);
+            canvas.addEventListener('mouseup', function(e) {
+                lilDiv.removeEventListener('mouseenter', hoverFunc);
+                e.target.removeEventListener('mousedown', clickHoldFunc);
+            })
+        });
         canvas.appendChild(lilDiv);
-        }
     }
-
+    
 })
-
-let stepList = () => {
-
-    for (j = 4; j < 1000; j++) {
-        if (Math.sqrt(j) % 1 == 0) {
-            let newOption = document.createElement('option');
-            newOption.innerHTML = j;
-            dataList.appendChild(newOption);
-        }
-    }
-
-}
-
-stepList();
-
-slider.list = dataList;
