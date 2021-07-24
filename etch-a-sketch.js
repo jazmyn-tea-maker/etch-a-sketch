203b420c5848cd03c947a50f14c9132df635758b
Jasmine <jasmineallen1107@gmail.com>
2021-07-24 10:03:55 -0500

    Added limit constructor to canvas event listener/Comments

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,14 +1,20 @@
-let slider = document.getElementById('myRange');
-slider.value = 4; // Default size.
-let sliderNum = 4; // Default size.
-let dataList = document.getElementById('dataList');
-let canvas = document.getElementById('canvas');
-let htmlStyle = document.getElementsByTagName('style');
-let divColor = document.getElementById('color-selector');
-let killScript;
+let setDefaultSlider = () => {
+    let slider = document.getElementById('boxRange');
+    slider.value = 4; // Default size.
+}
+setDefaultSlider();
 
-function throttled(delay, fn) { //Helps reduce ammount of times an event listener is called.
-    let lastCall = 0; // Also helps the whole thing not crash.
+let setDivColorDefault = () => {
+    let divColor = document.getElementById('color-selector');
+    divColor.value = '000000'; // Default color.
+}
+
+setDivColorDefault();
+
+
+//Performace functions.
+function throttled(delay, fn) { // Helps reduce ammount of times color change/mouseenter event listener is called.
+    let lastCall = 0;
     return function (...args) {
       const now = (new Date).getTime();
       if (now - lastCall < delay) {
@@ -20,7 +26,7 @@ function throttled(delay, fn) { //Helps reduce ammount of times an event listene
     }
   }
 
-  function debounced(delay, fn) { // Also helps reduces amount of event listener calls.
+  function debounced(delay, fn) { // Helps reduces amount of click event listener calls.
     let timerId;
     return function (...args) {
       if (timerId) {
@@ -33,8 +39,8 @@ function throttled(delay, fn) { //Helps reduce ammount of times an event listene
     }
   }
 
-  var factory = function(){
-    var time = 0, count = 0, difference = 0, queue = [];
+  function factory() { // Function call limiter. Only applied to event listener removal function.
+    let time = 0, count = 0, difference = 0, queue = [];
     return function limit(func){
         if(func) queue.push(func);
         difference = 1000 - (window.performance.now() - time);
@@ -47,11 +53,8 @@ function throttled(delay, fn) { //Helps reduce ammount of times an event listene
     };
 };
 
-var limited = factory();
-
-// This is to show a separator when waiting.
-var prevDate = window.performance.now(), difference;
-
+let sliderNum = 4; // Default size.
+let canvas = document.getElementById('canvas');
 
 // Setting up default size:
 let rowAndColumnNum = sliderNum;
@@ -69,16 +72,20 @@ for (k = 0; k < sliderNum ** 2; k++) {
 
 //Event Functions.
 function hoverFunc(e) { //Changes the color of the div.
+    let divColor = document.getElementById('color-selector');
     e.target.style.backgroundColor = divColor.value;
-} 
+}
 
 let removalFunc = () => {
+    let limited = factory(); //Constructor.
+    // This is to show a separator when waiting.
+    let prevDate = window.performance.now();
     limited(function() {
-        //
+        let difference;
         difference = window.performance.now() - prevDate;
         prevDate = window.performance.now();
-        if (difference > 200) console.log('wait');
-        //
+        if (difference > 60) console.log('wait');
+        //////////
         for (h = 0; h < canvas.childElementCount; h++) {
             canvas.children.item(h).removeEventListener('mouseenter', hoverFunc);
             canvas.children.item(h).removeEventListener('click', hoverFunc);
@@ -87,12 +94,22 @@ let removalFunc = () => {
 }
 
 canvas.addEventListener('mousedown', function clickHoldFunc() {
-    for (h = 0; h < canvas.childElementCount; h++) {
-        canvas.children.item(h).addEventListener('mouseenter', hoverFunc);
-        canvas.children.item(h).addEventListener('click', debounced(200, hoverFunc));
-    }
-    canvas.addEventListener('mouseup', throttled(200, removalFunc, {once: true}));
-    canvas.addEventListener('mouseleave', throttled(200, removalFunc, {once: true}));
+    let limited = factory(); //Constructor.
+    // This is to show a separator when waiting.
+    let prevDate = window.performance.now();
+    limited(function() {
+        let difference;
+        difference = window.performance.now() - prevDate;
+        prevDate = window.performance.now();
+        if (difference < 30) console.log('wait');
+        //////////
+        for (h = 0; h < canvas.childElementCount; h++) {
+            canvas.children.item(h).addEventListener('mouseenter', hoverFunc);
+            canvas.children.item(h).addEventListener('click', debounced(100, hoverFunc));
+        }
+        canvas.addEventListener('mouseup', throttled(200, removalFunc));
+        canvas.addEventListener('mouseleave', throttled(200, removalFunc));
+    })
 });
 
 
@@ -101,9 +118,9 @@ let userInput = document.getElementById('size-input');
 
 // If the user inputs text, use that.
 userInput.addEventListener('keydown', userCustomSize = (e) => {
-    console.log('userInput');
 
     if (e.key == 'Enter' || e.eventCode == 13) {
+
         let userInputSize = parseInt(userInput.value);// Turns the input into a number.
         if (userInputSize >= 91) {
             userInputSize = 90;
@@ -119,13 +136,9 @@ userInput.addEventListener('keydown', userCustomSize = (e) => {
                     background-color: white;
                     border: .1px solid #ededed;
                 `;
-                //PROBLEM: Lag! The canvas is creating itself over and over and over
-                // again with all these event listeners firing. It's taxing.
-                // Need to make separate loop.
                 
                 canvas.appendChild(lilDiv);
             }
-
             
         }
         userInput.value = ''; // Clears text input
@@ -134,9 +147,11 @@ userInput.addEventListener('keydown', userCustomSize = (e) => {
 });
 
 // If the user slides the bar, use the value from that.
+let slider = document.getElementById('boxRange');
+
 slider.addEventListener('input', changeCanvasSize = () => {
 
-    sliderNum = document.getElementById('myRange').value;
+    sliderNum = document.getElementById('boxRange').value;
     let rowAndColumnNum = sliderNum;
     canvas.style.gridTemplateRows = `repeat(${rowAndColumnNum}, 1fr)`;
     canvas.style.gridTemplateColumns = `repeat(${rowAndColumnNum}, 1fr)`;
